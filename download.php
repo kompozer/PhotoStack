051a3efe365e485777421b372f908c0745a535c7
Andreas Kompanez <kompozer@Andreas-Kompanezs-MacBook-Pro-15.local>
2010-01-10 22:01:21 +0100

    Forgot to add the code, allowes it to download an image

--- download.php
+++ download.php
@@ -1,7 +1,30 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once "includes/config.class.php";
 
+$gallery = null;
+$image = null;
+
+if(get_magic_quotes_gpc()) {
+    $gallery = stripslashes($_REQUEST['gallery']);
+    $image = stripslashes($_REQUEST['image']);
+} else {
+    $gallery = $_REQUEST['gallery'];
+    $image = $_REQUEST['image'];
+}
+
+if (null === $gallery || null === $image) {
+    die();
+}
+
+$config = new configuration();
+$galPath = realpath($config->pathto_galleries);
+$imgPath = realpath($config->pathto_galleries . $gallery . '/' . $image);
+
+if (!file_exists($imgPath)) {
+    die();
+}
+
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"$image\"");
+readfile($imgPath);
 ?>
